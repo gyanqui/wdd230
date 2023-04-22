8f576f54ce59cff0ba67102da368858de1f1d6e0
gyanqui <gyanqui3@gmail.com>
2023-04-22 14:48:22 -0500

    Create the home page

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// Get the currentYear
+const currentYear = new Date().getFullYear();
+document.getElementById('currentYear').textContent = currentYear;
+
+// Get the last update Modified date
+const lastUpdated = new Date(document.lastModified);
+const lastUpdatedElement = document.getElementById("lastUpdated");
+const formattedDate = lastUpdated.toLocaleDateString("en-US", {
+	year: "numeric",
+	month: "2-digit",
+	day: "2-digit",
+	hour: "2-digit",
+	minute: "2-digit",
+	second: "2-digit",
+	hour12: false 
+});
+lastUpdatedElement.textContent = formattedDate;
