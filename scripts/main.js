6efc77663160f81449524bbaab84fc3db19cfb90
gyanqui <gyanqui3@gmail.com>
2023-04-22 15:03:34 -0500

    Create home page

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
-// Get the currentYear
-const currentYear = new Date().getFullYear();
-document.getElementById('currentYear').textContent = currentYear;
+// Get the current year
+let currentYear = new Date().getFullYear();
+document.querySelector('#currentYear').textContent = currentYear;
 
 // Get the last update Modified date
 const lastUpdated = new Date(document.lastModified);
