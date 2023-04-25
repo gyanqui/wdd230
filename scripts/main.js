199d4d7cd93b509ce49af38d23ecaff236569dc1
gyanqui <gyanqui3@gmail.com>
2023-04-24 19:26:44 -0500

    Applying feedback from week1

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,4 @@ document.querySelector('#currentYear').textContent = currentYear;
 // Get the last update Modified date
 const lastUpdated = new Date(document.lastModified);
 const lastUpdatedElement = document.getElementById("lastUpdated");
-const formattedDate = lastUpdated.toLocaleDateString("en-US", {
-	year: "numeric",
-	month: "2-digit",
-	day: "2-digit",
-	hour: "2-digit",
-	minute: "2-digit",
-	second: "2-digit",
-	hour12: false 
-});
-lastUpdatedElement.textContent = formattedDate;
+lastUpdatedElement.textContent = document.lastModified;
