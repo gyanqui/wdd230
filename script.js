99d2b07f2e9e2c04c198136cf2086620c8d031f4
gyanqui <gyanqui3@gmail.com>
2023-05-18 23:14:32 -0500

    Adding home page to chamber

---
+++ script.js
@@ -0,0 +1,18 @@
+let currentYear = new Date().getFullYear();
+document.querySelector("#year").textContent = currentYear;
+document.querySelector("#lastUpdate").textContent = document.lastModified;
+
+
+const headerdate = document.querySelector("#date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+headerdate.innerHTML = `<strong>${fulldate}</strong>.`;
+
+
+function toggleMenu (){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggleMenu;
+
