02e59d58ce20ee812009b94a7543658c3d37c18c
gyanqui <gyanqui3@gmail.com>
2023-06-30 08:21:25 -0500

    Adding weather api to home page

--- chamber/scripts/joinbt.js
+++ chamber/scripts/joinbt.js
@@ -1,5 +1,5 @@
 const joinButton = document.getElementById("join-button");
 
 joinButton.addEventListener("click", function() {
-  // Functionality missing
+  window.open("../chamber/join.html", "_blank");
 });
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,18 +1,59 @@
-// Get the temperature and wind speed values
-const temperature = 8; 
-const windSpeed = 8; 
+const currentTemp = document.querySelector('#current-temp');
+const weatherFigure = document.querySelector('.weather-figure');
+const captionDesc = document.querySelector('.fig-p');
+const windSpeedElement = document.getElementById('windSpeed');
+const windChillElement = document.getElementById('windChill');
 
-// Check if the values meet the specification limits for wind chill calculation
-const elements = {
-    temperature: document.getElementById("temperature"),
-    windSpeed: document.getElementById("wind-speed"),
-    windChill: document.getElementById("wind-chill")
-};
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=cusco&units=metric&appid=b0560e33a2a4e47b4de1d7ac806338f4';
 
-elements.temperature.textContent = temperature;
-elements.windSpeed.textContent = windSpeed;
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw new Error('Error fetching weather data:', response.status);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-elements.windChill.textContent = (temperature <= 10 && windSpeed > 4.8) ? calculateWindChill(temperature, windSpeed).toFixed(1) : "N/A";
+apiFetch();
+
+function capitalizeEachWord(string) {
+    return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+function displayResults(weatherData) {
+    const celsiusTemp = Math.round(weatherData.main.temp);
+    const kmPerHour = (weatherData.wind.speed * 3.6).toFixed(1);
+
+    currentTemp.innerHTML = `<strong>${celsiusTemp}</strong>°C`;
+
+    const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+    const desc = capitalizeEachWord(weatherData.weather[0].description);
+
+    const weatherIcon = document.createElement('img');
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    weatherIcon.setAttribute('id', 'weather-icon');
+
+    weatherFigure.appendChild(weatherIcon);
+    captionDesc.textContent = desc;
+
+    const windSpeed = kmPerHour;
+    const temperature = celsiusTemp;
+    if (temperature <= 10 && windSpeed > 4.8) {
+        const windChill = calculateWindChill(temperature, windSpeed).toFixed(1);
+        windChillElement.textContent = `${windChill}°C`;
+    } else {
+        windChillElement.textContent = 'N/A';
+    }
+
+    windSpeedElement.textContent = `${windSpeed} km/h`;
+}
 
 function calculateWindChill(temperature, windSpeed) {
     return 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
