0cfb52dd1017a8466d78171cc864e54a3148da37
gyanqui <gyanqui3@gmail.com>
2023-07-02 10:22:02 -0500

    Cleanning up pages

---
+++ chamber/scripts/button.js
@@ -0,0 +1,11 @@
+const joinButton = document.getElementById("join-button");
+
+joinButton.addEventListener("click", function() {
+  window.open("../chamber/join.html", "_blank");
+});
+
+const moreInfoButton = document.getElementById('more-info');
+
+  moreInfoButton.addEventListener('click', function() {
+    window.open('https://www.xapiriground.org/', "_blank");
+  });
--- chamber/scripts/joinbt.js
+++
@@ -1,5 +0,0 @@
-const joinButton = document.getElementById("join-button");
-
-joinButton.addEventListener("click", function() {
-  window.open("../chamber/join.html", "_blank");
-});
