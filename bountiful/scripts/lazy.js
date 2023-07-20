3e159ebf3485c0e0d53f2ba5b95b1eb2145441e6
gyanqui <gyanqui3@gmail.com>
2023-07-20 06:37:42 -0500

    Cleaning Code

---
+++ bountiful/scripts/lazy.js
@@ -0,0 +1,34 @@
+const images = document.querySelectorAll ("img[data-src]");
+
+function preloadImage (img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
+
+    img.src = src;
+    img.removeAttribute("data-src");
+};
+
+const imgOptions = {
+    threshold: 0, 
+    rootMargin: "0px 0px 20px 0px"
+};
+
+const imgObserver = new IntersectionObserver((items,imgObserver) => {
+    
+    items.forEach (item => {
+        if (!item.isIntersecting) {
+            return;
+        }
+        else {
+            preloadImage(item.target);
+            imgObserver.unobserve(item.target);
+        }
+    })
+
+}, imgOptions);
+
+images.forEach (image => {
+    imgObserver.observe (image);
+})
