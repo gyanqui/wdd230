1a63de7a2cd79bfa09bef4a0f65958ac5ddc9ce4
gyanqui <gyanqui3@gmail.com>
2023-07-20 07:02:31 -0500

    Clean up comments

--- bountiful/scripts/makemix.js
+++ bountiful/scripts/makemix.js
@@ -31,67 +31,67 @@ fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
         console.error('Error fetching fruit data:', error);
     });
 
-// document.getElementById('drink-form').addEventListener('submit', function (event) {
+document.getElementById('drink-form').addEventListener('submit', function (event) {
 
-//     const firstName = document.getElementById('fname').value;
-//     const email = document.getElementById('email').value;
-//     const phone = document.getElementById('phone').value;
-//     const fruit1 = document.getElementById('fruit1').value;
-//     const fruit2 = document.getElementById('fruit2').value;
-//     const fruit3 = document.getElementById('fruit3').value;
-//     const instructions = document.getElementById('instructions').value;
+    const firstName = document.getElementById('fname').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const fruit1 = document.getElementById('fruit1').value;
+    const fruit2 = document.getElementById('fruit2').value;
+    const fruit3 = document.getElementById('fruit3').value;
+    const instructions = document.getElementById('instructions').value;
 
-//     const selectedFruits = [
-//         findFruitById(fruit1),
-//         findFruitById(fruit2),
-//         findFruitById(fruit3)
-//     ];
+    const selectedFruits = [
+        findFruitById(fruit1),
+        findFruitById(fruit2),
+        findFruitById(fruit3)
+    ];
 
-//     let totalCarbohydrates = 0;
-//     let totalProtein = 0;
-//     let totalFat = 0;
-//     let totalCalories = 0;
-//     let totalSugar = 0;
+    let totalCarbohydrates = 0;
+    let totalProtein = 0;
+    let totalFat = 0;
+    let totalCalories = 0;
+    let totalSugar = 0;
 
-//     for (const fruit of selectedFruits) {
-//         totalCarbohydrates += fruit.nutritions.carbohydrates;
-//         totalProtein += fruit.nutritions.protein;
-//         totalFat += fruit.nutritions.fat;
-//         totalCalories += fruit.nutritions.calories;
-//         totalSugar += fruit.nutritions.sugar;
-//     }
+    for (const fruit of selectedFruits) {
+        totalCarbohydrates += fruit.nutritions.carbohydrates;
+        totalProtein += fruit.nutritions.protein;
+        totalFat += fruit.nutritions.fat;
+        totalCalories += fruit.nutritions.calories;
+        totalSugar += fruit.nutritions.sugar;
+    }
 
-//     const currentDate = new Date().toLocaleDateString();
-//     const orderDetails = `
-//         <h3>Order Details:</h3>
-//         <p><strong>First Name:</strong> ${firstName}</p>
-//         <p><strong>Email:</strong> ${email}</p>
-//         <p><strong>Phone Number:</strong> ${phone}</p>
-//         <p><strong>Fruit 1:</strong> ${findFruitNameById(fruit1)}</p>
-//         <p><strong>Fruit 2:</strong> ${findFruitNameById(fruit2)}</p>
-//         <p><strong>Fruit 3:</strong> ${findFruitNameById(fruit3)}</p>
-//         <p><strong>Special Instructions:</strong> ${instructions}</p>
-//         <p><strong>Order Date:</strong> ${currentDate}</p>
-//     `;
+    const currentDate = new Date().toLocaleDateString();
+    const orderDetails = `
+        <h3>Order Details:</h3>
+        <p><strong>First Name:</strong> ${firstName}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone Number:</strong> ${phone}</p>
+        <p><strong>Fruit 1:</strong> ${findFruitNameById(fruit1)}</p>
+        <p><strong>Fruit 2:</strong> ${findFruitNameById(fruit2)}</p>
+        <p><strong>Fruit 3:</strong> ${findFruitNameById(fruit3)}</p>
+        <p><strong>Special Instructions:</strong> ${instructions}</p>
+        <p><strong>Order Date:</strong> ${currentDate}</p>
+    `;
 
-//     const nutritionalInfo = `
-//         <h3>Nutritional Information:</h3>
-//         <p><strong>Total Carbohydrates:</strong> ${totalCarbohydrates.toFixed(2)} g</p>
-//         <p><strong>Total Protein:</strong> ${totalProtein.toFixed(2)} g</p>
-//         <p><strong>Total Fat:</strong> ${totalFat.toFixed(2)} g</p>
-//         <p><strong>Total Calories:</strong> ${totalCalories} cal</p>
-//         <p><strong>Total Sugar:</strong> ${totalSugar.toFixed(2)} g</p>
-//     `;
+    const nutritionalInfo = `
+        <h3>Nutritional Information:</h3>
+        <p><strong>Total Carbohydrates:</strong> ${totalCarbohydrates.toFixed(2)} g</p>
+        <p><strong>Total Protein:</strong> ${totalProtein.toFixed(2)} g</p>
+        <p><strong>Total Fat:</strong> ${totalFat.toFixed(2)} g</p>
+        <p><strong>Total Calories:</strong> ${totalCalories} cal</p>
+        <p><strong>Total Sugar:</strong> ${totalSugar.toFixed(2)} g</p>
+    `;
 
-//     document.getElementById('order-details').innerHTML = orderDetails;
-//     document.getElementById('nutritional-info').innerHTML = nutritionalInfo;
-// });
+    document.getElementById('order-details').innerHTML = orderDetails;
+    document.getElementById('nutritional-info').innerHTML = nutritionalInfo;
+});
 
-// function findFruitById(id) {
-//     return fruitData.find(fruit => fruit.id === parseInt(id));
-// }
+function findFruitById(id) {
+    return fruitData.find(fruit => fruit.id === parseInt(id));
+}
 
-// function findFruitNameById(id) {
-//     const fruit = findFruitById(id);
-//     return fruit ? fruit.name : 'Unknown Fruit';
-// }
+function findFruitNameById(id) {
+    const fruit = findFruitById(id);
+    return fruit ? fruit.name : 'Unknown Fruit';
+}
